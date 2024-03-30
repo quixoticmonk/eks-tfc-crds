df89e53b6b890a1b459b16269437b83fa7871342
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2024-03-30 18:51:56 -0400

    test: with checks and conditions

---
+++ checks.tf
@@ -0,0 +1,10 @@
+check "health_check" {
+  data "http" "terraform_io" {
+    url = "https://www.terraform.io"
+  }
+
+  assert {
+    condition = data.http.terraform_io.status_code == 200
+    error_message = "${data.http.terraform_io.url} returned an unhealthy status code"
+  }
+}
--- data.tf
+++ data.tf
@@ -13,3 +13,14 @@ locals {
     product  = "webinar-example"
   }
 }
+
+data "http" "terraform_io" {
+  url = "https://www.terraform.io"
+
+  lifecycle {
+    postcondition {
+        condition = self.status_code == 200
+        error_message = "${self.url} returned an unhealthy status code"
+    }
+  }
+}
