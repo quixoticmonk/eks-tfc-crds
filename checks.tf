f110e83c1920d2b4fb8dd3eb8e2a03707c973096
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2024-04-02 19:52:58 -0400

    Update checks.tf
    Updated checks to see when it will run again

--- checks.tf
+++ checks.tf
@@ -8,3 +8,20 @@ check "health_check" {
     error_message = "${data.http.terraform_io.url} returned an unhealthy status code"
   }
 }
+
+
+check "cluster_status" {
+
+  assert {
+    condition = module.eks.cluster_status== "ACTIVE"
+    error_message = "The cluster status is ${module.eks.cluster_status}"
+  }
+}
+
+check "cluster_version" {
+
+  assert {
+    condition = module.eks.cluster_version== var.cluster_version
+    error_message = "The cluster version is ${module.eks.cluster_version}"
+  }
+}
