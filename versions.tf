1459e815c9bd26875cb5cbc0e8b7dcd0e10b8a2d
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2024-04-07 10:19:31 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -6,10 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.40"
     }
-    # kubernetes = {
-    #   source = "hashicorp/kubernetes"
-    #   version = "2.27.0"
-    # }
+    kubernetes = {
+       source = "hashicorp/kubernetes"
+       version = "2.27.0"
+     }
   }
 }
 
@@ -18,8 +18,10 @@ provider "aws" {
 }
 
 
-# provider "kubernetes" {
-#   host = module.eks.cluster_endpoint
+provider "kubernetes" {
+  host = module.eks.cluster_endpoint
 
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-# }
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  #token                  = data.aws_eks_cluster_auth.cluster.token
+  #load_config_file       = false
+}
