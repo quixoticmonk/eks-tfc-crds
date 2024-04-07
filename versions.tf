af14b291f746ceb42b49f39a586ce5963199064f
quixoticmonk <manuchandrasekhar@gmail.com>
2024-04-07 10:47:33 -0400

    fix: remove k8s provider

--- versions.tf
+++ versions.tf
@@ -6,22 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.40"
     }
-    kubernetes = {
-       source = "hashicorp/kubernetes"
-       version = "2.27.0"
-     }
+
   }
 }
 
 provider "aws" {
   region = local.region
-}
-
-
-provider "kubernetes" {
-  host = module.eks.cluster_endpoint
-
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  #token                  = data.aws_eks_cluster_auth.cluster.token
-  #load_config_file       = false
 }
