51fa85e3e68ebc19c39e6ad51c1d31708f881f4d
quixoticmonk <manuchandrasekhar@gmail.com>
2024-04-08 23:38:54 -0400

    fix: cluster references in provider

--- versions.tf
+++ versions.tf
@@ -35,14 +35,14 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.this.cluster_endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.cluster_certificate_authority_data)
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this.cluster_name]
+      args = ["eks", "get-token", "--cluster-name", local.name]
     }
   }
 }
@@ -50,15 +50,15 @@ provider "helm" {
 
 provider "kubectl" {
   apply_retry_count      = 5
-  host                   = data.aws_eks_cluster.this.cluster_endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.cluster_certificate_authority_data)
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   load_config_file       = false
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this.cluster_name]
+    args = ["eks", "get-token", "--cluster-name", local.name]
   }
 }
 
