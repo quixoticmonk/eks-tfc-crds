f50dda941e1fa14a709ae06b97fb429db2c6d7a3
quixoticmonk <manuchandrasekhar@gmail.com>
2024-04-09 21:05:51 -0400

    test: pixie integration

--- main.tf
+++ main.tf
@@ -23,6 +23,9 @@ resource "kubernetes_manifest" "nribundle_nribundle_sample" {
       "kubeEvents" = {
         "enabled" = true
       }
+      "newrelic-pixie" = {
+        "enabled" = true
+      }
       "newrelic-infrastructure" = {
         "enabled" = true
         "privileged" = true
