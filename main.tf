0a99752057d23ae86da99d4d7b274cacc1a78043
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2024-04-26 14:37:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,44 +1,44 @@
 
-resource "kubernetes_manifest" "nribundle_nribundle_sample" {
-  depends_on = [ data.aws_eks_cluster.this]
-  manifest = {
-    "apiVersion" = "newrelic.com/v1alpha1"
-    "kind" = "NRIBundle"
-    "metadata" = {
-      "name" = "nribundle-sample"
-      "namespace" = "newrelic"
-    }
-    "spec" = {
-      "global" = {
-        "cluster" = local.name
-        "licenseKey" = var.nr_license_key
-        "lowDataMode" = true
-      }
-      "kube-state-metrics" = {
-        "enabled" = true
-        "image" = {
-          "tag" = "v2.10.0"
-        }
-      }
-      "kubeEvents" = {
-        "enabled" = true
-      }
-      "newrelic-infrastructure" = {
-        "enabled" = true
-        "privileged" = true
-      }
-      "newrelic-prometheus-agent" = {
-        "config" = {
-          "kubernetes" = {
-            "integrations_filter" = {
-              "enabled" = true
-            }
-          }
-        }
-        "enabled" = true
-        "lowDataMode" = true
-      }
-    }
-  }
-}
+// resource "kubernetes_manifest" "nribundle_nribundle_sample" {
+//   depends_on = [ data.aws_eks_cluster.this]
+//   manifest = {
+//     "apiVersion" = "newrelic.com/v1alpha1"
+//     "kind" = "NRIBundle"
+//     "metadata" = {
+//       "name" = "nribundle-sample"
+//       "namespace" = "newrelic"
+//     }
+//     "spec" = {
+//       "global" = {
+//         "cluster" = local.name
+//         "licenseKey" = var.nr_license_key
+//         "lowDataMode" = true
+//       }
+//       "kube-state-metrics" = {
+//         "enabled" = true
+//         "image" = {
+//           "tag" = "v2.10.0"
+//         }
+//       }
+//       "kubeEvents" = {
+//         "enabled" = true
+//       }
+//       "newrelic-infrastructure" = {
+//         "enabled" = true
+//         "privileged" = true
+//       }
+//       "newrelic-prometheus-agent" = {
+//         "config" = {
+//           "kubernetes" = {
+//             "integrations_filter" = {
+//               "enabled" = true
+//             }
+//           }
+//         }
+//         "enabled" = true
+//         "lowDataMode" = true
+//       }
+//     }
+//   }
+// }
 
