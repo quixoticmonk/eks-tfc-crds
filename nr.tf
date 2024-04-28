1c0606f9279fc7692b210f7a7d62c09a98f30216
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2024-04-28 19:40:21 -0400

    Added dashboard json

--- nr.tf
+++ nr.tf
@@ -1,5 +1,5 @@
 resource "newrelic_one_dashboard_json" "k8s_dash" {
-     json = templatefile("${path.module}/json/infra_dashboard.json",{account_id=var.nr_account_id})
+     json = templatefile("${path.module}/json/infra_dashboard.tftpl",{account_id=var.nr_account_id})
 }
 
 resource "newrelic_one_dashboard_json" "k8s_logs" {
