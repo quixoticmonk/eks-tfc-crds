64bec39ac71afb2a3b1a584b1d076c543a8923ed
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2024-04-28 19:56:31 -0400

    updated templates

--- nr.tf
+++ nr.tf
@@ -1,7 +1,7 @@
 resource "newrelic_one_dashboard_json" "k8s_dash" {
-     json = templatefile("${path.module}/infra_dashboard.tftpl",{account_id=var.nr_account_id})
+     json = templatefile("${path.module}/templates/infra_dashboard.tftpl",{account_id=var.nr_account_id})
 }
 
 resource "newrelic_one_dashboard_json" "k8s_logs" {
-     json = templatefile("${path.module}/log_ingestion.tftpl",{account_id=var.nr_account_id})
+     json = templatefile("${path.module}/templates/log_ingestion.tftpl",{account_id=var.nr_account_id})
 }
